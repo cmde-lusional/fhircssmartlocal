5a926c3a0a554085e736c500d28efc3a164bba50
Moritz Rosar <moritzrosar@hotmail.de>
2023-02-10 15:08:07 +0800

    add webserver with useUrl() and receive url funtion

--- fhircssmartlocal/Program.cs
+++ fhircssmartlocal/Program.cs
@@ -1,14 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Threading.Channels;
 using fhircssmartlocal;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
 using Hl7.Fhir.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-Console.WriteLine("Hello, World!");
+//Setting a dynamic web server url while picking a free port number
+builder.WebHost.UseUrls("http://127.0.0.1:0");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Start();
+
+//print the urls that have been picked by the UseUrls function above 
+Console.WriteLine("URLs:");
+
+int urlCounter = 0;
+foreach (var weburl in app.Urls)
+{
+    Console.WriteLine($"url {urlCounter, 3}: {weburl}");
+}
 
 // connection dic for fire server
 Dictionary<string, string> _fhirServers = new Dictionary<string, string>()
@@ -38,3 +58,5 @@ else
     Console.WriteLine($"token url: {tokenUrl}");
     return 0;
 }
+
+
